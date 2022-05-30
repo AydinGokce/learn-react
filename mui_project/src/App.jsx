9e1bac49c82670a42c66320b70b3d642d82df7a1
aydin <aydingokce@vt.edu>
2022-05-30 19:07:37 -0400

    learning mui

---
+++ mui_project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Typography , AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container} from '@material-ui/core';
+
+const App = () => {
+  return (
+    <div>
+        <Typography variant='h1'>Hello, World!</Typography>
+    </div>
+  );
+}
+
+export default App;
